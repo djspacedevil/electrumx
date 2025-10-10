8369471857c3daf6146c38065624c9a4391bfc72
djspacedevil <github.7zd@op-sys.de>
2025-10-10 15:57:11 +0200

    Update coins.py

--- src/electrumx/lib/coins.py
+++ src/electrumx/lib/coins.py
@@ -4133,6 +4133,8 @@ class DeutscheEMark(Coin):
     NAME = "Deutsche eMark"
     SHORTNAME = "DEM"
     NET = "mainnet"
+    DAEMON = daemon.LegacyRPCDaemon
+    DESERIALIZER = lib_tx.DeserializerTxTime
 
     # Ökonomie/Info
     COIN = 100_000_000           # 1 DEM = 1e8 base units
@@ -4144,10 +4146,10 @@ class DeutscheEMark(Coin):
 
     # Base58 Präfixe (MAINNET)
     # DEM nutzt i.d.R. P2PKH mit führendem 'N' -> 0x35 (53 dez).
-    P2PKH_VERBYTE = 0x35
+    P2PKH_VERBYTE = bytes.fromhex("35")
     # Häufig wie Bitcoin; bitte bei Bedarf gegen chainparams prüfen:
-    P2SH_VERBYTE  = 0x05
-    WIF_BYTE      = 0x80
+    P2SH_VERBYTE  = (bytes.fromhex("05"),)
+    WIF_BYTE      = bytes.fromhex("80")
 
     # optionale Heuristik (start-up hints)
     TX_COUNT = 1
@@ -4159,11 +4161,13 @@ class DeutscheEMarkTestnet(DeutscheEMark):
     NAME = "Deutsche eMark"
     SHORTNAME = "DEM"
     NET = "testnet"
+    DAEMON = daemon.LegacyRPCDaemon
+    DESERIALIZER = lib_tx.DeserializerTxTime
 
     # Testnet-Genesis
     GENESIS_HASH = "0000752b9e9142c3ec3d9e84f86feb59ce9e7e44d64996b5a021dbf55e3b4355"
 
     # Standard-BTC-Testnet-Präfixe (passen zu 'm...' Adressen)
-    P2PKH_VERBYTE = 0x6F   # 111 dez -> 'm'/'n'
-    P2SH_VERBYTE  = 0xC4   # 196 dez -> '2'
-    WIF_BYTE      = 0xEF   # 239 dez
+    P2PKH_VERBYTE = bytes.fromhex("6f")   # 111 dez -> 'm'/'n'
+    P2SH_VERBYTE  = (bytes.fromhex("c4"),)   # 196 dez -> '2'
+    WIF_BYTE      = bytes.fromhex("ef")   # 239 dez
